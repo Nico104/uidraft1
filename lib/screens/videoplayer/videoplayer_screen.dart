7453f4b0a5a19a8981bebc290b0245283fa1b4c6
Nico104 <nicolascarraro104@gmail.com>
2021-10-11 22:43:52 +0200

    videoplayer autoplay

--- lib/screens/videoplayer/videoplayer_screen.dart
+++ lib/screens/videoplayer/videoplayer_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uidraft1/utils/responsive/responsive_builder_widget.dart';
 import 'package:uidraft1/widgets/navbar/profile/navbar_large_profile_widget.dart';
+import 'package:uidraft1/widgets/videoplayer/video_player_test2_widget.dart';
 import 'package:uidraft1/widgets/videoplayer/video_player_widget.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
@@ -19,7 +20,8 @@ class _VideoPlayerState extends State<VideoPlayerScreen> {
       largeScreen: Material(
         child: Stack(
           alignment: Alignment.topCenter,
-          children: [ChapterVideoPlayer(), const NavBarLargeProfile()],
+          // children: [ChapterVideoPlayer(), const NavBarLargeProfile()],
+          children: [VideoPlayerApp(), const NavBarLargeProfile()],
         ),
       ),
       veryLargeScreen: Text("veryLargeScreen"),
