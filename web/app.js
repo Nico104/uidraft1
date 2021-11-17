2a91a2b2e9c9bb694427fe03fbd93e5fc309e877
Nico104 <nicolascarraro104@gmail.com>
2021-11-17 09:12:31 +0100

    esaceptest

--- web/app.js
+++ web/app.js
@@ -6,13 +6,14 @@ window.logger = (flutter_value) => {
     console.log({ js_context: this, flutter_value });
 }
 
-window.preventDefault();
+// window.preventDefault();
 
 document.addEventListener("keydown", function (e) {
     var charCode = e.charCode || e.keyCode || e.which;
     if (charCode == 27) {
         // alert("Escape is not suppressed for lightbox!");
         console.log("Escape oh yeah");
-        return false;
+        // e.preventDefault();
+        // return false;
     }
 });
